ebb95b96b55b2f5ee7af5d80bf50b0dcacf7c7ff
Kevin Winter <kevinrwinter@protonmail.com>
2023-03-16 10:51:07 +0200

    Update delete controllers to destroy additionals

--- app/Http/Controllers/Vessels/DeleteVessel.php
+++ app/Http/Controllers/Vessels/DeleteVessel.php
@@ -8,6 +8,7 @@ use App\Models\Vessel;
 use App\Models\VesselCabin;
 use App\Models\CrewCabin;
 use App\Helpers\ApiResponse;
+use App\Models\VesselCabinAdditionals;
 
 class DeleteVessel extends Controller
 {
@@ -25,9 +26,17 @@ class DeleteVessel extends Controller
             return ApiResponse::error('Vessel not found');
         }
 
+        // Get all cabins that belong to a vessel
+        $vesselCabin = VesselCabin::where('vessel_id', $id)->get();
+        // Loop over its cabins and delete its additionals and itself (cabin)
+        foreach ($vesselCabin as $key => $cabin) {
+            // Delete the additionals
+            VesselCabinAdditionals::where('cabin_id', $cabin->id)->delete();
+            // Delete the cabin
+            $cabin->delete();
+        }
+        // Delete the vessel
         $vessel->delete();
-        VesselCabin::where('vessel_id', $id)->delete();
-        CrewCabin::where('vessel_id', $id)->delete();
 
         return ApiResponse::success($vessel, 'Vessel deleted successfully');
     }
