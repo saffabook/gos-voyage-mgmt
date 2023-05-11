91d5f28d2588dd1470d3357f85425da66010f295
Kevin Winter <kevinrwinter@protonmail.com>
2023-05-11 13:15:56 +0300

    Add modifications to UpdateVoyagePort controller

--- app/Http/Controllers/Voyages/UpdateVoyagePort.php
+++ app/Http/Controllers/Voyages/UpdateVoyagePort.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Helpers\ApiResponse;
 use App\Models\VoyagePort;
+use Illuminate\Validation\Rule;
 
 class UpdateVoyagePort extends Controller
 {
@@ -18,28 +19,37 @@ class UpdateVoyagePort extends Controller
      */
     public function __invoke(Request $request)
     {
-        $companyId = 0;
-
         $validatedData = Validator::make($request->all(), [
-            'id'          => 'required|integer|exists:voyage_ports,id',
-            'title'       => 'required|string|unique:voyage_ports|max:255'.$request->id,
+            'id'    => 'required|integer|exists:voyage_ports,id',
+            'title' => [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('voyage_ports')
+                    ->where('companyId', $request->companyId)
+                    ->ignore($request->id)
+            ],
             'description' => 'string|between:30,600',
             'directions'  => 'string|max:255'
         ], [
-            'title.unique' => 'A port with that name already exists.'
+            'title.unique' => 'You have already created a port with that name.'
         ]);
 
         if ($validatedData->fails()) {
             return ApiResponse::error($validatedData->messages());
         }
 
-        $port = VoyagePort::where('companyId', $companyId)
+        $port = VoyagePort::where('companyId', $request->input('companyId'))
             ->find($request->input('id'));
+
+        if (empty($port)) {
+            return ApiResponse::error('Port not found.');
+        }
 
         $port->fill($validatedData->validated());
 
         $port->save();
 
-        return ApiResponse::success($port, 'The voyage port has been updated');
+        return ApiResponse::success($port, 'The port has been updated.');
     }
 }
