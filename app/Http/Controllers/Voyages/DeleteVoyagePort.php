2a202fc281f18de659abf8552d23fd2cd723f549
Kevin Winter <kevinrwinter@protonmail.com>
2023-05-16 10:46:42 +0300

    Update variable name for route parameter

--- app/Http/Controllers/Voyages/DeleteVoyagePort.php
+++ app/Http/Controllers/Voyages/DeleteVoyagePort.php
@@ -17,11 +17,11 @@ class DeleteVoyagePort extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke($id, Request $request)
+    public function __invoke($portId, Request $request)
     {
         $port = VoyagePort::where('companyId', $request->companyId)
-                          ->where('id', $id)
-                          ->find($id);
+                          ->where('id', $portId)
+                          ->find($portId);
 
         if (empty($port)) {
             return ApiResponse::error('Port not found');
@@ -30,8 +30,8 @@ class DeleteVoyagePort extends Controller
         if (!$request->input('forceAction')) {
             $today = Carbon::now();
 
-            $portIsActive = VesselVoyage::where('embarkPortId', $id)
-                                        ->orWhere('disembarkPortId', $id)
+            $portIsActive = VesselVoyage::where('embarkPortId', $portId)
+                                        ->orWhere('disembarkPortId', $portId)
                                         ->whereDate('startDate', '<=', $today)
                                         ->whereDate('endDate', '>=', $today)
                                         ->exists();
