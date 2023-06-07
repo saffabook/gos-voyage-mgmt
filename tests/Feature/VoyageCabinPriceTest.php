2fd09a2ef7b15d620e4c9936b99be41f7a67fe0f
Kevin Winter <kevinrwinter@protonmail.com>
2023-06-07 15:55:02 +0300

    Add checks and validation to update price
    Updated controller with validation and conditional logic. Updated testing service class for in/active voyages. Created tests associated with updating cabin prices.

--- tests/Feature/VoyageCabinPriceTest.php
+++ tests/Feature/VoyageCabinPriceTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\VoyageCabinPrice;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use Tests\CommonSetups\Voyages\VoyageTestSetupService;
@@ -144,5 +145,300 @@ class VoyageCabinPriceTest extends TestCase
             $jsonResponse['error']['currency'][0],
             'The currency field is required.'
         );
+    }
+
+    /**
+     * When updating a cabin price, ensure modifying inactive voyage yields no errors.
+     *
+     * @return void
+     */
+    public function testUserCanUpdateTheirOwnPriceWhenVoyageIsNotActive()
+    {
+        $companyId = '1';
+        $testDataVesselCabin = VoyageTestSetupService::createTestDataVesselCabin($companyId);
+        $voyage = VoyageTestSetupService::createTestDataVoyageInactive($companyId);
+
+        $price = VoyageCabinPrice::create([
+            'cabinId'    => $testDataVesselCabin['cabin']->id,
+            'voyageId'   => $voyage->id,
+            'currency'   => 'EUR',
+            'priceMinor' => '11000',
+            'companyId'  => $companyId
+
+        ]);
+
+        $request = [
+            'currency'   => 'USD',
+            'priceMinor' => '99000',
+            'companyId'  => $companyId
+        ];
+
+        $jsonResponse = $this->postJson('/api/prices/update/'.$price->id, $request);
+
+        $jsonResponse->assertStatus(200)
+            ->assertJson(['data' => $request]);
+
+        $this->assertDatabaseHas('voyage_cabin_prices', $request);
+    }
+
+    /**
+     * Ensure user cannot update another company's cabin price.
+     *
+     * @return void
+     */
+    public function testUserCannotUpdateOtherUsersPrices()
+    {
+        $companyId = '1';
+        $testDataVesselCabin = VoyageTestSetupService::createTestDataVesselCabin($companyId);
+        $voyage = VoyageTestSetupService::createTestDataVoyage($companyId);
+
+        $price = VoyageCabinPrice::create([
+            'cabinId'    => $testDataVesselCabin['cabin']->id,
+            'voyageId'   => $voyage->id,
+            'currency'   => 'EUR',
+            'priceMinor' => '11000',
+            'companyId'  => $companyId
+        ]);
+
+        $request = [
+            'currency'   => 'EUR',
+            'priceMinor' => '99999',
+            'companyId'  => '99'
+        ];
+
+        $jsonResponse = $this->postJson('/api/prices/update/'.$price->id, $request);
+
+        $jsonResponse->assertStatus(422)
+            ->assertJson([
+                'error' => 'Price not found.'
+            ]);
+    }
+
+    /**
+     * Ensure user cannot update cabin price if voyage is active.
+     *
+     * @return void
+     */
+    public function testUserCannotUpdatePriceIfVoyageIsActive()
+    {
+        $companyId = '1';
+        $cabin = VoyageTestSetupService::createTestDataVesselCabin($companyId);
+        $voyage = VoyageTestSetupService::createTestDataVoyage($companyId);
+
+        $price = VoyageCabinPrice::create([
+            'cabinId'    => $cabin['cabin']->id,
+            'voyageId'   => $voyage->id,
+            'currency'   => 'EUR',
+            'priceMinor' => '11000',
+            'companyId'  => $companyId
+
+        ]);
+
+        $request = [
+            'currency'   => 'EUR',
+            'priceMinor' => '99999',
+            'companyId'  => $companyId
+        ];
+
+        $jsonResponse = $this->postJson('/api/prices/update/'.$price->id, $request);
+
+        $jsonResponse->assertStatus(422)
+            ->assertJson([
+                'error' => 'Voyage is active. Do you want to add a discount price?'
+            ]);
+
+        $this->assertDatabaseMissing('voyage_cabin_prices', $request);
+    }
+
+    /**
+     * Ensure user can update cabin price with confirmation, if voyage is active.
+     *
+     * @return void
+     */
+    public function testUserCanUpdatePriceWithForceAction()
+    {
+        $companyId = '1';
+        $testDataVesselCabin = VoyageTestSetupService::createTestDataVesselCabin($companyId);
+        $voyage = VoyageTestSetupService::createTestDataVoyage($companyId);
+
+        $price = VoyageCabinPrice::create([
+            'cabinId'    => $testDataVesselCabin['cabin']->id,
+            'voyageId'   => $voyage->id,
+            'currency'   => 'EUR',
+            'priceMinor' => '11000',
+            'companyId'  => $companyId
+        ]);
+
+        $request = [
+            'currency'    => 'EUR',
+            'priceMinor'  => '99999',
+            'companyId'   => $companyId,
+            'forceAction' => true
+        ];
+
+        $jsonResponse = $this->postJson('/api/prices/update/'.$price->id, $request);
+
+        $jsonResponse->assertStatus(200)
+            ->assertJsonPath('data.message', 'The price has been updated.');
+
+        $this->assertDatabaseHas('voyage_cabin_prices', [
+            'id'         => $price->id,
+            'currency'   => $request['currency'],
+            'priceMinor' => $request['priceMinor']
+        ]);
+    }
+
+    /**
+     * Ensure price is minor.
+     *
+     * @return void
+     */
+    public function testUserCannotUpdatePriceIfValueIsNotMinor()
+    {
+        $companyId = '1';
+        $testDataVesselCabin = VoyageTestSetupService::createTestDataVesselCabin($companyId);
+        $voyage = VoyageTestSetupService::createTestDataVoyage($companyId);
+
+        $price = VoyageCabinPrice::create([
+            'cabinId'    => $testDataVesselCabin['cabin']->id,
+            'voyageId'   => $voyage->id,
+            'currency'   => 'EUR',
+            'priceMinor' => '11000',
+            'companyId'  => $companyId
+
+        ]);
+
+        $request = [
+            'currency'    => 'EUR',
+            'priceMinor'  => '999.99',
+            'companyId'   => $companyId,
+            'forceAction' => true
+        ];
+
+        $jsonResponse = $this->postJson('/api/prices/update/'.$price->id, $request);
+
+        $jsonResponse->assertStatus(422);
+
+        $this->assertSame(
+            $jsonResponse['error']['priceMinor'][0],
+            'The price minor must be an integer.'
+        );
+
+        $this->assertDatabaseMissing('voyage_cabin_prices', [
+            'priceMinor' => $request['priceMinor']
+        ]);
+    }
+
+    /**
+     * Ensure currency is present.
+     *
+     * @return void
+     */
+    public function testUserCannotUpdatePriceWithoutCurrency()
+    {
+        $companyId = '1';
+        $testDataVesselCabin = VoyageTestSetupService::createTestDataVesselCabin($companyId);
+        $voyage = VoyageTestSetupService::createTestDataVoyage($companyId);
+
+        $price = VoyageCabinPrice::create([
+            'cabinId'    => $testDataVesselCabin['cabin']->id,
+            'voyageId'   => $voyage->id,
+            'currency'   => 'EUR',
+            'priceMinor' => '11000',
+            'companyId'  => $companyId
+
+        ]);
+
+        $request = [
+            'priceMinor'  => '99999',
+            'companyId'   => $companyId,
+            'forceAction' => true
+        ];
+
+        $jsonResponse = $this->postJson('/api/prices/update/'.$price->id, $request);
+
+        $jsonResponse->assertStatus(422);
+
+        $this->assertSame(
+            $jsonResponse['error']['currency'][0],
+            'The currency field is required.'
+        );
+
+        $this->assertDatabaseMissing('voyage_cabin_prices', $request);
+    }
+
+    /**
+     * Ensure discount price is not set higher than price.
+     *
+     * @return void
+     */
+    public function testUserCannotUpdatePriceIfDiscountIsGreaterThanOriginal()
+    {
+        $companyId = '1';
+        $testDataVesselCabin = VoyageTestSetupService::createTestDataVesselCabin($companyId);
+        $voyage = VoyageTestSetupService::createTestDataVoyage($companyId);
+
+        $price = VoyageCabinPrice::create([
+            'cabinId'    => $testDataVesselCabin['cabin']->id,
+            'voyageId'   => $voyage->id,
+            'currency'   => 'EUR',
+            'priceMinor' => '11000',
+            'companyId'  => $companyId
+
+        ]);
+
+        $request = [
+            'currency'             => 'EUR',
+            'discountedPriceMinor' => '99999',
+            'companyId'            => $companyId,
+        ];
+
+        $jsonResponse = $this->postJson('/api/prices/update/'.$price->id, $request);
+
+        $jsonResponse->assertStatus(422)
+            ->assertJson([
+                'error' => 'Discounted price must be less than original price.'
+            ]);
+
+        $this->assertDatabaseMissing('voyage_cabin_prices', [
+            'discountedPriceMinor' => $request['discountedPriceMinor']
+        ]);
+    }
+
+    /**
+     * If changing price without force action, respond with
+     * suggesting to add a discount price.
+     *
+     * @return void
+     */
+    public function testUserMessageToAddDiscountPriceIfForceActionIsNotPresent()
+    {
+        $companyId = '1';
+        $testDataVesselCabin = VoyageTestSetupService::createTestDataVesselCabin($companyId);
+        $voyage = VoyageTestSetupService::createTestDataVoyage($companyId);
+
+        $price = VoyageCabinPrice::create([
+            'cabinId'    => $testDataVesselCabin['cabin']->id,
+            'voyageId'   => $voyage->id,
+            'currency'   => 'EUR',
+            'priceMinor' => '11000',
+            'companyId'  => $companyId
+
+        ]);
+
+        $request = [
+            'currency'   => 'EUR',
+            'priceMinor' => '99999',
+            'companyId'  => $companyId,
+        ];
+
+        $jsonResponse = $this->postJson('/api/prices/update/'.$price->id, $request);
+
+        $jsonResponse->assertStatus(422)
+            ->assertJson([
+                'error' => 'Voyage is active. Do you want to add a discount price?'
+            ]);
+
+        $this->assertDatabaseMissing('voyage_cabin_prices', $request);
     }
 }
