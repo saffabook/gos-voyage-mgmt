3ed2559b6a0d932c7eb473afc59f1a4a788e4646
Kevin Winter <kevinrwinter@protonmail.com>
2023-07-25 17:33:06 +0300

    Remove DeleteVoyagePrice controller and update API routes

--- routes/api.php
+++ routes/api.php
@@ -86,6 +86,7 @@ Route::group([
     ], function () {
     Route::post('create', Controllers\Voyages\CreateVesselVoyage::class);
     Route::post('/get/{id}', Controllers\Voyages\GetVesselVoyage::class);
+    Route::post('/delete/{id}', Controllers\Voyages\DeleteVesselVoyage::class);
 });
 
 Route::group([
@@ -103,5 +104,4 @@ Route::group([
     Route::post('create', Controllers\Prices\CreateVoyagePrice::class);
     Route::post('/', Controllers\Prices\ListVoyagePrices::class);
     Route::post('update/{id}', Controllers\Prices\UpdateVoyagePrice::class);
-    Route::post('delete/{id}', Controllers\Prices\DeleteVoyagePrice::class);
 });
